f20d3d9ad3820e379795e8a2afeb94e2c1614238
Joshua San Jose <forimbajosh21@gmail.com>
2021-09-29 20:32:23 +0800

    add module resolver

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,19 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@app': './src/app',
+            '@components': './src/common/components',
+            '@utils': './src/common/utils',
+          },
+        },
+      ],
+    ],
   }
 }
